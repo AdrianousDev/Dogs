ae1bd3fbc77433bb5dcdbaff36ca6da910313d05
bruno j a <brunoadrianousz@gmail.com>
2026-08-04 14:28:13 -0300

    feat: add header component styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import Dogs from "../assets/dogs.svg?react";
 
 const Header = () => {
     return (
-        <div className={styles.header}>
-            <nav className="container">
-                <Link to="/">Home</Link>
-                <Link to="/login">Login / Criar</Link>
+        <header className={styles.header}>
+            <nav className={`container ${styles.nav}`}>
+                <Link to="/" aria-label="Dogs - Home" className={styles.logo}>
+                    <Dogs />
+                </Link>
+                <Link to="/login" className={styles.login}>
+                    Login / Criar
+                </Link>
             </nav>
-        </div>
+        </header>
     );
 };
 
