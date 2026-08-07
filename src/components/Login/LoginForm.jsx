ddb43c7c61fa563a41e4b8481be59930d74719b3
bruno j a <brunoadrianousz@gmail.com>
2026-08-07 11:03:12 -0300

    feat: create Error component and initial style to login form

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -3,6 +3,9 @@ import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../hooks/useForm";
 import useUser from "../../contexts/user/useUser";
+import Error from "../Helper/Error";
+import styles from "./LoginForm.module.css";
+import stylesBtn from "../Forms/Button.module.css";
 
 const LoginForm = () => {
     const username = useForm();
@@ -19,9 +22,9 @@ const LoginForm = () => {
     };
 
     return (
-        <section>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
+        <section className="animeLeft">
+            <h1 className="title">Login</h1>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <Input
                     label="Usuário"
                     type="text"
@@ -42,10 +45,20 @@ const LoginForm = () => {
                     <Button>Entrar</Button>
                 )}
 
-                {error && <p>{error}</p>}
+                <Error error={error} />
             </form>
 
-            <Link to="/login/criar">Cadastro</Link>
+            <Link className={styles.perdeu} to="/login/perdeu">
+                Perdeu a Senha?
+            </Link>
+
+            <div className={styles.cadastro}>
+                <h2 className={styles.subtitle}>Cadastre-se</h2>
+                <p>Ainda não possui conta? Cadastre-se no site.</p>
+                <Link className={stylesBtn.button} to="/login/criar">
+                    Cadastro
+                </Link>
+            </div>
         </section>
     );
 };
