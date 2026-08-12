76f04852d151034804e8f32a1bf1251443b6f0ba
bruno j a <brunoadrianousz@gmail.com>
2026-08-12 14:12:05 -0300

    feat: add user stats page with external graph lib, using lazy and suspense to render them

--- src/components/User/UserStats.jsx
+++ src/components/User/UserStats.jsx
@@ -1,11 +1,35 @@
+import { lazy, Suspense, useEffect } from "react";
+import useFetch from "../../hooks/useFetch";
 import Head from "../Helper/Head";
+import { STATS_GET } from "../../api";
+import Loading from "../Helper/Loading";
+import Error from "../Helper/Error";
+const UserStatsGraphs = lazy(() => import("./UserStatsGraphs"));
 
 const UserStats = () => {
-    return (
-        <div>
-            <Head title="Estatísticas" />
-        </div>
-    );
+    const { data, loading, error, request } = useFetch();
+
+    useEffect(() => {
+        const getData = async () => {
+            const token = window.localStorage.getItem("token");
+
+            const { url, options } = STATS_GET(token);
+
+            await request(url, options);
+        };
+        getData();
+    }, [request]);
+
+    if (loading) return <Loading />;
+    if (error) return <Error error={error} />;
+    if (data)
+        return (
+            <Suspense fallback={<div></div>}>
+                <Head title="Estatísticas" />
+                <UserStatsGraphs data={data} />
+            </Suspense>
+        );
+    else return null;
 };
 
 export default UserStats;
